6b0fc9e35d26f0a3fddae2ada80b1a2daef6726f
Aymeric <Aymkdn@users.noreply.github.com>
2020-03-06 09:02:43 +0100

    v1.1.0

--- README.md
+++ README.md
@@ -44,7 +44,6 @@ build.configureWebpack.mergeConfig({
 build.initialize(gulp);
 ```
 
-
 ## How it works
 
 The plugin will analyze your code, in files with extension `.js`, `.jsx`, `.ts` and `.vue`, to detect the below line:
@@ -59,4 +58,69 @@ import list from 'sharepointplus/es5/lists/list.js' // if `$SP().list()` is used
 import get from 'sharepointplus/es5/lists/get.js' // if `$SP().list().get()` is used in the analyzed file
 import getPeopleLookup from 'sharepointplus/es5/utils/getPeopleLookup.js'  // if `$SP().getPeopleLookup()` is used in the analyzed file
 const $SP = spInit({'list':list, 'get':get, 'getPeopleLookup':getPeopleLookup });
+```
+
+### Complex project
+
+If your project has several files using SharepointPlus, but you only want to load it as a global object with only what matters, then you can use `// sp-loader-path: "./path/"` in your main file.
+
+Example with the below hierarchy:
+```markdown
+.
++-- classes/
+| |-- Files.js
+| +-- Folders.js
++-- components/
+| |-- App.vue
+| |-- Header.vue
+| +-- Footer.vue
++-- plugins/
+| +-- vuetify.js
++-- store/
+| |-- files.js
+| |-- folders.js
+| +-- list.js
++-- index.js
+```
+
+We have several calls of `$SP()` all other the place, but we want all of them to be loaded by `index.js`. We will only call `import $SP from 'sharepointplus'` in `index.js` and we'll use `sp-loader-path` as the below:
+```javascript
+// index.js – before sharepoint-loader has analyzed it
+
+// Load $SP modules from files in the below path,
+//   using glob patterns (see https://github.com/isaacs/node-glob#readme)
+// sp-loader-path: "./classes/**"
+// sp-loader-path: "./components/**"
+// sp-loader-path: "./store/**"
+import $SP from 'sharepointplus'
+global.$SP = $SP; // to make sure it will be available everywhere
+```
+
+Once `sharepointplus-loader` has analyzed the file, it will convert it to load all the necessary modules:
+```javascript
+// index.js – after sharepoint-loader has analyzed it
+
+// all the modules from the sub files have been listed and are loaded
+import spInit from 'sharepointplus/es5/init.js'
+import cleanResult from 'sharepointplus/es5/lists/cleanResult.js'
+import getLookup from 'sharepointplus/es5/utils/getLookup.js'
+import getPeopleLookup from 'sharepointplus/es5/utils/getPeopleLookup.js'
+import toDate from 'sharepointplus/es5/utils/toDate.js'
+import showModalDialog from 'sharepointplus/es5/modals/showModalDialog.js'
+import closeModalDialog from 'sharepointplus/es5/modals/closeModalDialog.js'
+import ajax from 'sharepointplus/es5/utils/ajax.js'
+import list from 'sharepointplus/es5/lists/list.js'
+import add from 'sharepointplus/es5/lists/add.js'
+import get from 'sharepointplus/es5/lists/get.js'
+import workflowStatusToText from 'sharepointplus/es5/utils/workflowStatusToText.js'
+import getWorkflowID from 'sharepointplus/es5/lists/getWorkflowID.js'
+import startWorkflow from 'sharepointplus/es5/lists/startWorkflow.js'
+import remove from 'sharepointplus/es5/lists/remove.js'
+import update from 'sharepointplus/es5/lists/update.js'
+import getContentTypeInfo from 'sharepointplus/es5/lists/getContentTypeInfo.js'
+import toSPDate from 'sharepointplus/es5/utils/toSPDate.js'
+import webService from 'sharepointplus/es5/utils/webService.js'
+import createFile from 'sharepointplus/es5/files/createFile.js'
+const $SP = spInit({cleanResult:cleanResult, getLookup:getLookup, getPeopleLookup:getPeopleLookup, toDate:toDate, showModalDialog:showModalDialog, closeModalDialog:closeModalDialog, ajax:ajax, list:list, add:add, get:get, workflowStatusToText:workflowStatusToText, getWorkflowID:getWorkflowID, startWorkflow:startWorkflow, remove:remove, update:update, getContentTypeInfo:getContentTypeInfo, toSPDate:toSPDate, webService:webService, createFile:createFile});
+global.$SP = $SP; // to make sure it will be available everywhere
 ```
--- loader.js
+++ loader.js
@@ -1,34 +1,72 @@
+var glob = require("glob");
+var fs = require('fs');
+var path = require('path');
+
+function findModules (content, modules) {
+  // Find any calls to $SP() that is not inside comments
+  var mtch = content.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g, '').match(/(.*)?\$SP\(\).*/g);
+  modules = modules || new Set();
+
+  // for each $SP() found, we identify the module
+  if (mtch) {
+    mtch.forEach(function(m) {
+      var res = m.match(/\$SP\(\)\.((list)\([^\)]+\)\.)?([^\(]+)/);
+      if (res && res.length===4) {
+        if (res[2]) {
+          modules.add(res[2]);
+        }
+        if (res[3]) {
+          modules.add(res[3]);
+        }
+      }
+    });
+  }
+
+  return modules;
+}
+
 module.exports = function(content, mapSource) {
   this.async();
   this.cacheable();
 
-  if (content && /\$SP()/.test(content) && /import \$SP from 'sharepointplus'/.test(content) && this.resourcePath && !/sharepointplus/i.test(this.resourcePath)) {
-    var modules = new Set();
-    // Find any calls to $SP() that is not inside comments
-    var mtch = content.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g, '').match(/(.*)?\$SP\(\).*/g);
+  if (content && (/import \$SP from .sharepointplus./.test(content) || /import * as \$SP from .sharepointplus./.test(content)) && this.resourcePath && !/sharepointplus/i.test(this.resourcePath)) {
+    // find the $SP() in the current file
+    var modules = findModules(content);
+    var resourcePath = this.resourcePath;
 
-    // for each $SP() found, we identify the module
-    if (mtch) {
-      mtch.forEach(function(m) {
-        var res = m.match(/\$SP\(\)\.((list)\([^\)]+\)\.)?([^\(]+)/);
-        if (res && res.length===4) {
-          if (res[2]) modules.add(res[2]);
-          if (res[3]) modules.add(res[3]);
+    // if we have "sp-loader-path:" then we'll scan the resources to find modules in the related resources
+    var mtchPath = content.match(/sp-loader-path:\s?["|'].*["|']/g);
+    if (mtchPath) {
+      mtchPath.forEach(function(m) {
+        // find the path
+        var pathToResource = m.match(/sp-loader-path:\s?["|'](.*)["|']/);
+        if (pathToResource && pathToResource.length===2) {
+          // read files in the path
+          var cwd = path.dirname(resourcePath).replace(/\\/g, '/'); // replace \ from Windows to /
+          var files = glob.sync(cwd + '/' + pathToResource[1]); // options don't work?!
+          files.forEach(function(filePath) {
+            // check if the path is a file or a folder
+            if (fs.lstatSync(filePath).isFile()) {
+              var fileContent = fs.readFileSync(filePath, 'utf-8');
+              modules = findModules(fileContent, modules);
+            }
+          })
         }
-      });
+      })
+    }
 
-      // replace "import \$SP from 'sharepointplus'" with individual modules
-      if (modules.size > 0) {
-        var paths = require('./path.json');
-        var newContent = ["import spInit from 'sharepointplus/es5/init.js'"];
-        var init = [];
-        modules.forEach(function(mod) {
-          newContent.push("import "+mod+" from 'sharepointplus/es5"+paths[mod]+"'");
-          init.push(mod+':'+mod);
-        });
-        newContent.push("const $SP = spInit({"+init.join(', ')+"});");
-        content = content.replace(/import \$SP from 'sharepointplus'/, newContent.join("\n"));
-      }
+    // replace "import $SP from 'sharepointplus'" with individual modules
+    if (modules.size > 0) {
+      var paths = require('./path.json');
+      var newContent = ["import spInit from 'sharepointplus/es5/init.js'"];
+      var init = [];
+      modules.forEach(function(mod) {
+        newContent.push("import "+mod+" from 'sharepointplus/es5"+paths[mod]+"'");
+        init.push(mod+':'+mod);
+      });
+      newContent.push("const $SP = spInit({"+init.join(', ')+"});");
+      console.log(newContent.join("\n"));
+      content = content.replace(/import \$SP from .sharepointplus.|import \* as \$SP from .sharepointplus./, newContent.join("\n"));
     }
   }
   this.callback(null, content, mapSource);
